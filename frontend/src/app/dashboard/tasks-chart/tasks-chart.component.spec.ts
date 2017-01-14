9d4feff126217cea89d19d5a217a03519438efc0
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2017-01-14 10:50:37 -0300

    Added tasks-chart component

---
+++ frontend/src/app/dashboard/tasks-chart/tasks-chart.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TasksChartComponent } from './tasks-chart.component';
+
+describe('TasksChartComponent', () => {
+  let component: TasksChartComponent;
+  let fixture: ComponentFixture<TasksChartComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TasksChartComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TasksChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
