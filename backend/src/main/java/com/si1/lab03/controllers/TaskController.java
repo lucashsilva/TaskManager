668643ed76e510dc967f929642b842378a79866f
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2017-01-23 17:27:21 -0300

    Added priority and categories

--- backend/src/main/java/com/si1/lab03/controllers/TaskController.java
+++ backend/src/main/java/com/si1/lab03/controllers/TaskController.java
@@ -116,5 +116,5 @@ public class TaskController {
 			return new ResponseEntity<Task>(HttpStatus.FORBIDDEN);
 		}
 	}
-	
+
 }
---
+++ backend/src/main/java/com/si1/lab03/models/Priority.java
@@ -0,0 +1,5 @@
+package com.si1.lab03.models;
+
+public enum Priority {
+	LOW, NORMAL, HIGH
+}
